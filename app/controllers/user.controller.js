d75ddb7e929bda14a90a9e9bc25c9e7aa5e1c83a
Sanket Pimple <55106986+sankhusky@users.noreply.github.com>
2020-10-01 11:17:06 -0400

    Closes #6, #4 and #2 : Completed API endpoints, added validation and encrytion (#8)
    * Adding a basic GitHub Actions workflow
    
    * Adding the basic node webapp structure
    
    * Added bcrypt, code for encrypting and decrypting the password
    
    * Adding the basic node webapp structure
    
    * Added bcrypt, code for encrypting and decrypting the password
    
    * Created the api endpoint for /self, added the code for authorization and encryption
    
    * Closes #4 and #6 : Added basic auth and created APIs for GET and PUT
    
    Added basic authentication, used it in handling the GET and PUT /self endpoints. PUT is used for updating. Errors have been handled.
    
    * Fixed error handling, refined some code
    
    * Added password validation as per NIST guidelines
    
    * Adding validations for empty user data
    
    * Validation checks for empty auth fields

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,15 @@
 const db = require("../models");
-// const User = require("../models/user.model.js");
 const User = db.user;
 const Op = db.Sequelize.Op;
-const bcrypt = require('bcrypt');
+
+var passwordValidator = require('password-validator');
+const commonPasswordList = require('fxa-common-password-list');
+
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
+//TODO: Fix and review all the error codes and error messages below. handle more cases!!! There are always more cases to handle!
+
 // console.log(db.Sequelize.models);
 // Create and Save a new user
 /*{
@@ -13,61 +19,91 @@ const saltRounds = 10;
   "email_address": "jane.doe@example.com"
 }*/
 exports.create = (req, res) => {
+  // console.log(req.headers);
 
-  console.log(req.headers);
-    // Validate request
-    // if (!req.body.title) {
-    //   res.status(400).send({
-    //     message: "Content can not be empty!"
-    //   });
-    //   return;
-    // }
-  
-    // console.log(db);
-    // Create a Tutorial
+  //Validate password
+  var schema = new passwordValidator();
 
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-      if (err) {
-          return res.status(500).json({
-              statusCode: '409',
-              message: 'Internal Server Error occurred'             
-          });
-      }
-      else {
-        //TODO: validate password and insert on success
+  schema
+    .is().min(9) // Minimum length 9
+    .is().max(150) // Maximum length 150
+    .has().uppercase() // Must have uppercase letters
+    .has().lowercase() // Must have lowercase letters
+    .has().digits(2) // Must have at least 2 digits
+    .has().symbols(1)
+    .has().not().spaces() // Should not have spaces
+    .is().not().oneOf(['Passw0rd', 'Password123', 'password', '1234567890']); // Blacklist these values
 
-        const user = {
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          password: hash,
-          username: req.body.username,
-        };
-      
-        // Save Tutorial in the database
-        User.create(user)
-          .then(data => {
-            let resp = data.dataValues;
-            delete resp.password;
-            res.send(resp);
-          })
-          .catch(err => {
-            res.status(500).send({
-              message:
-                err.message || "Some error occurred while creating the Tutorial."
-            });
-          });
-
-      }
-
+  //check for empty fields
+  if (req.body.first_name == undefined || req.body.first_name == '' ||
+    req.body.last_name == undefined || req.body.last_name == '' ||
+    req.body.password == undefined || req.body.password == '' ||
+    req.body.username == undefined || req.body.username == '') {
+    return res.status(400).json({
+      message: "Mandatory fields (first name, last name, username, password) cannot be empty!"
     });
-    
-  };
+  }
+
+  let pass = req.body.password;
+  if ((schema.validate(pass) /*&& !commonPasswordList(pass)*/ ) == false) {
+    //password is invalid
+    return res.status(400).json({
+      message: "Error: Weak / Invalid password.\nPlease use a strong and uncommon password with length more than 8, containing uppercase, lowercase, two digits, a symbol and no spaces"
+    });
+
+  }
+
+  bcrypt.hash(pass, saltRounds, (err, hash) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Error occurred while password encryption:" + err.message
+      });
+    } else {
+
+      const user = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: hash,
+        username: req.body.username,
+      };
+
+      // Save Tutorial in the database
+      User.create(user)
+        .then((data) => {
+          let resp = data.dataValues;
+          delete resp.password;
+          res.status(201).send({
+            message: "User Created!",
+            data: resp
+          });
+        })
+        .catch((err) => {
+
+          console.log(err.errors[0].message);
+          if (err.errors[0].message == 'username must be unique') {
+            res.status(400).send({
+              message: "Error: The user already exists!",
+            });
+
+          } else if (err.message == 'Validation isEmail on username failed' || err.message.includes('isEmail')) {
+            res.status(400).send({
+              message: "Validation Error: Please enter a valid email ID!"
+            });
+          } else {
+            res.status(400).send({
+              message: "Error:" + err.message,
+            });
+          }
+        });
+    }
+  });
+};
 
 // // Retrieve all Tutorials from the database.
 // exports.findAll = (req, res) => {
 //     const title = req.query.title;
 //     var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  
+
 //     Tutorial.findAll({ where: condition })
 //       .then(data => {
 //         res.send(data);
@@ -80,121 +116,214 @@ exports.create = (req, res) => {
 //       });
 //   };
 
-// // Find a single Tutorial with an id
-// exports.findOne = (req, res) => {
-//   const id = req.params.id;
+// Find a single Tutorial with an id
+exports.findSelf = (req, res) => {
+  const cred = getCredentialsFromAuth(req.headers.authorization);
+  if((cred.username==undefined || cred.username=='')
+    || cred.password==undefined || cred.password==''){
+      return res.status(400).send({
+        message: "Error: Auth username, password cannot be empty"
+      });
+    }
+  getHash(cred.username)
+    .then((hash) => {
+      console.log(hash);
+      bcrypt
+        .compare(cred.password, hash)
+        .then((result) => {
+          // console.log("in then");
+          // console.log(result);
+          console.log("isValid" + result);
+          if (result == true) {
+            User.findOne({
+                where: {
+                  username: cred.username,
+                },
+                attributes: {
+                  exclude: ["password"],
+                },
+              })
+              .then((data) => {
+                res.send(data);
+              })
+              .catch((err) => {
+                if (err.message == " ")
+                  res.status(400).send({
+                    message: "error " + err,
+                  });
+              });
+          } else {
+            //wrong password
+            return res.status(401).send({
+              message: "Error: Wrong password",
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: "Error occurred:" + err,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(401).send({
+        message: err,
+      });
+    });
+};
 
-//   Tutorial.findByPk(id)
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error retrieving Tutorial with id=" + id
-//       });
-//     });
-// };
-
-// // Update a Tutorial by the id in the request
-// exports.update = (req, res) => {
-//   const id = req.params.id;
-
-//   Tutorial.update(req.body, {
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Tutorial was updated successfully."
-//         });
-//       } else {
-//         res.send({
-//           message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error updating Tutorial with id=" + id
-//       });
-//     });
-// };
-
-
-// // Delete a Tutorial with the specified id in the request
-// exports.delete = (req, res) => {
-//   const id = req.params.id;
-
-//   Tutorial.destroy({
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Tutorial was deleted successfully!"
-//         });
-//       } else {
-//         res.send({
-//           message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Could not delete Tutorial with id=" + id
-//       });
-//     });
-// };
-
-// // Delete all Tutorials from the database.
-// exports.deleteAll = (req, res) => {
-//   Tutorial.destroy({
-//     where: {},
-//     truncate: false
-//   })
-//     .then(nums => {
-//       res.send({ message: `${nums} Tutorials were deleted successfully!` });
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while removing all tutorials."
-//       });
-//     });
-// };
-
-// // Find all published Tutorials
-// exports.findAllPublished = (req, res) => {
-//   Tutorial.findAll({ where: { published: true } })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while retrieving tutorials."
-//       });
-//     });
-// };
-
-function validatePassword(email,suppliedPassword){
-const hash = await User.findAll({
-  where: {
-    username: email
+// Update the user
+exports.updateUser = (req, res) => {
+  const cred = getCredentialsFromAuth(req.headers.authorization);
+  if((cred.username==undefined || cred.username=='')
+  || cred.password==undefined || cred.password==''){
+    return res.status(400).send({
+      message: "Error: Auth username, password cannot be empty"
+    });
   }
-});
-  // Load hash from your password DB.
-bcrypt.compare(suppliedPassword, hash, function(err, result) {
-  //TODO: handle error and set result
-  if(err){
-    return false;
-  }else if(result){
-    return true;
-  }else{
-    return false;
-  }
-  
-  // result == true
-});
+  let updateObject = req.body;
+  let userEmail = "";
+  if ("username" in updateObject) {
 
+    if (updateObject.username != cred.username) {
+      return res.status(400).send({
+        message: "Error: Please specify correct usernames!"
+      });
+      //unreachable code below
+      // throw new Error("Please specify correct usernames!");
+    }
+    userEmail = updateObject.username;
+    delete updateObject.username;
+  } else {
+    userEmail = cred.username;
+  }
+  getHash(userEmail)
+    .then((hash) => {
+      console.log(hash);
+      bcrypt
+        .compare(cred.password, hash)
+        .then((result) => {
+          // console.log("in then");
+          // console.log(result);
+          console.log("isValid" + result);
+          if (result == true) {
+            let notAllowedFields = [];
+            for (const key in updateObject) {
+              if (
+                key != "first_name" &&
+                key != "last_name" &&
+                key != "password"
+              ) {
+                notAllowedFields.push(key);
+              }
+            }
+            if (notAllowedFields.length > 0) {
+              // res.status(400).send({
+              //   message: "Update Failed: Fields not allowed:  " + notAllowedFields
+              // });
+              throw new Error("Update Failed: Fields not allowed:  " + notAllowedFields);
+
+            }
+            console.log("update object");
+            console.log(updateObject);
+
+            if ("password" in updateObject) {
+              bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                if (err) {
+                  // return res.status(500).json({
+                  //   message: "Hash encryption failed:" + err,
+                  // });
+                  throw new Error("Hash encryption failed:" + err);
+                } else {
+                  updateObject.password = hash;
+                  //update the user here
+                  User.update(updateObject, {
+                      where: {
+                        username: userEmail,
+                      },
+                    })
+                    .then(() => {
+                      res.status(204).send({
+                        message: "User Updated successfully",
+                      });
+                    })
+                    .catch((err) => {
+                      res.status(400).send({
+                        message: "error " + err,
+                      });
+                    });
+                }
+              });
+            } else {
+              //update body doesn't contain password field:
+
+              //update the user here
+              User.update(updateObject, {
+                  where: {
+                    username: userEmail,
+                  },
+                })
+                .then(() => {
+                  res.status(204).send({
+                    message: "User Updated successfully",
+                  });
+                })
+                .catch((err) => {
+                  res.status(400).send({
+                    message: "error " + err,
+                  });
+                });
+
+
+            }
+          } else {
+            //wrong password
+            return res.status(401).send({
+              message: "Error: Wrong password",
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: err,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "Error:" + err,
+      });
+    });
+};
+
+async function getHash(email) {
+  // let hash = null;
+  const data = await User.findAll({
+    attributes: ["password"],
+    where: {
+      username: email,
+    },
+  });
+  // console.log(data);
+  if (data === undefined || data.length == 0)
+    throw new Error("Wrong Username!");
+
+  const hash = data[0].dataValues.password;
+
+  return hash;
+}
+
+//TODO: Add validation for empty username password
+function getCredentialsFromAuth(authHeader) {
+  const b64auth = (authHeader || "").split(" ")[1] || "";
+  const strauth = Buffer.from(b64auth, "base64").toString();
+  const splitIndex = strauth.indexOf(":");
+  const login = strauth.substring(0, splitIndex);
+  const password = strauth.substring(splitIndex + 1);
+  let cred = {
+    username: login,
+    password: password,
+  };
+
+  return cred;
 }
