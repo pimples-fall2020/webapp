ff24098c17eeaf282737fa75f1a1f93e0b7e336d
Sanket Pimple <pimple.s@northeastern.edu>
2020-11-04 12:48:49 -0500

    Adding async commands for node

--- codedeploy_scripts/start_server.sh
+++ codedeploy_scripts/start_server.sh
@@ -3,4 +3,6 @@ cd /home/ubuntu/webapp
 # set any env variables
 # export NODE_ENV=staging
 
-npm run start
+# npm run start
+sudo kill -9 $(sudo lsof -t -i:8080);
+sudo nohup npm start > /dev/null 2> /dev/null < /dev/null &
