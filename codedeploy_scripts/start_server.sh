d4f5750bbb54a06470969c4debd1a1a81ace1d12
Sanket Pimple <pimple.s@northeastern.edu>
2020-11-11 14:32:15 -0500

    Adding winston config, cloudwatch setup

--- codedeploy_scripts/start_server.sh
+++ codedeploy_scripts/start_server.sh
@@ -3,6 +3,12 @@ cd /home/ubuntu/webapp
 # set any env variables
 # export NODE_ENV=staging
 
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:./cloudwatch-config.json \
+-s
+
 # npm run start
 sudo kill -9 $(sudo lsof -t -i:8080);
 sudo nohup npm start > /dev/null 2> /dev/null < /dev/null &
