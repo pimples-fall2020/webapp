99164a3c2f88369d6e18ca670dac73dd143aa8f3
Sanket Pimple <pimple.s@northeastern.edu>
2020-11-11 18:23:56 -0500

    Changed logger config-added timestamp

--- app/config/logger.config.js
+++ app/config/logger.config.js
@@ -1,11 +1,20 @@
 const winston = require('winston');
-<<<<<<< HEAD
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+  });
 
 const logger = winston.createLogger({
     // level: 'info',
     // format: winston.format.json(),
-    format: winston.format.simple(),
+    // format: winston.format.simple(),
+    format: combine(        
+        timestamp(),
+        myFormat
+      ),
     // defaultMeta: { service: 'user-service' },
+    
     transports: [
         //
         // - Write all logs with level `error` and below to `error.log`
@@ -18,37 +27,14 @@ const logger = winston.createLogger({
     ]
 });
 
-=======
- 
-const logger = winston.createLogger({
-  // level: 'info',
-  // format: winston.format.json(),
-  // defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'csye6225-webapp.log' }),
-  ],
-});
- 
->>>>>>> Adding winston config, cloudwatch setup
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.IS_EC2 != 'true') {
-<<<<<<< HEAD
     logger.add(new winston.transports.Console({
         format: winston.format.simple(),
     }));
-=======
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
->>>>>>> Adding winston config, cloudwatch setup
 }
 
 module.exports = logger;
