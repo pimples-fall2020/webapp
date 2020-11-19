4d57c1789a9947a1882fc7a5542e4585725dc95a
Sanket Pimple <pimple.s@northeastern.edu>
2020-11-18 19:03:17 -0500

    Fixing logger config path in both server.js files

--- server-ec2.js
+++ server-ec2.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const logger = require('../config/logger.config');
+const logger = require('./app/config/logger.config');
 const app = express();
 
 var corsOptions = {
