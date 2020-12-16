e693d6bd676893ca8aef99d56611b50d2baa2169
Sanket Pimple <pimple.s@northeastern.edu>
2020-12-16 00:22:59 -0500

    Fix: Connection to RDS over SSL

--- app/config/db.config-ec2.js
+++ app/config/db.config-ec2.js
@@ -5,11 +5,17 @@
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME,
     dialect: "mysql",
-    ssl: 'Amazon RDS',
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        ssl: 'Amazon RDS',
+        rejectUnauthorized: true
+        // ca: [rdsCa]
+      }
     }
   };
